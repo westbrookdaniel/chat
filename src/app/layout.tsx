d0c68360c2f4fe189393023b87b11da0b3ad71cf
westy12dan <westy12dan@gmail.com>
2025-05-04 12:10:02 +0800

    favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,6 +25,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link
+          rel="icon"
+          type="image/svg+xml"
+          href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 width=%22256%22 height=%22256%22 viewBox=%220 0 100 100%22><rect x=%2225%22 y=%2280%22 width=%2255%22 height=%224%22 rx=%220%22 fill=%22rgb(163, 163, 163)%22></rect></svg>"
+        />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased bg-background`}
       >
